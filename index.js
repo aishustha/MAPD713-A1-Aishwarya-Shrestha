fb7c5a3f1ff0c1235dcfe23cb949ce4f91c353dc
Aishwarya Shrestha <aishwaryashrestha4064@gmail.com>
2021-10-03 15:59:18 -0400

    add HTTP GET Requests

--- index.js
+++ index.js
@@ -16,6 +16,23 @@ let Database = [];
 
 //server to handle request and response
 var server = http.createServer(async function (request, response) {
+   //requests - GET / POST / DELETE
+   if (request.url == "/" && request.method == "GET") {
+    response.writeHead(200, { "Content-Type": "text/plain" }); 
+    response.end("Namaste!");
+  }
+
+  //handle  HTTP GET request
+  else if (request.url == "/images" && request.method == "GET") {
+    if (Database.length == 0) {
+      response.writeHead(200, { "Content-Type": "text/plain" }); 
+      response.end("Image Array field is empty.");
+    } else {
+      response.writeHead(200, { "Content-Type": "application/json" }); 
+      const responsePayload = { data: Database };
+      response.end(JSON.stringify(responsePayload));
+    }
+  }
 });
 
 //server is listening at http://127.0.0.1:8000
