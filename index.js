e141f34d851caa078884c99a30b3ec348db84907
Aishwarya Shrestha <aishwaryashrestha4064@gmail.com>
2021-10-03 16:19:03 -0400

    Added Request Counters and Endpoints info

--- index.js
+++ index.js
@@ -14,9 +14,15 @@ const http = require("http");
 //implemented data storage
 let Database = [];
 
+//2 request counters
+let getIncrement = 0;
+let postIncrement = 0;
+
 //server to handle request and response
 var server = http.createServer(async function (request, response) {
    //requests - GET / POST / DELETE
+   //request counter for GET requests
+   getIncrement = getIncrement + 1;
    if (request.url == "/" && request.method == "GET") {
     response.writeHead(200, { "Content-Type": "text/plain" }); 
     response.end("Namaste!");
@@ -24,6 +30,8 @@ var server = http.createServer(async function (request, response) {
 
   //handle  HTTP GET request
   else if (request.url == "/images" && request.method == "GET") {
+    //request counter for GET requests
+    getIncrement = getIncrement + 1;
     if (Database.length == 0) {
       response.writeHead(200, { "Content-Type": "text/plain" }); 
       response.end("Image Array field is empty.");
@@ -36,6 +44,8 @@ var server = http.createServer(async function (request, response) {
 
   //handle HTTP POST request with JSON Payload
   else if (request.url == "/images" && request.method == "POST") {
+    //request counter for POST requests
+    postIncrement = postIncrement + 1;
     //stores in memory json payload received in POST request
     //Return a list of all images information as a response to GET request
     const loading = [];
@@ -54,9 +64,14 @@ var server = http.createServer(async function (request, response) {
     response.end("Image is cleared from Database.");
   }
       
+  //log request counters values every request
+  console.log(`Processed Request Count --> Get: ${getIncrement}, Post: ${postIncrement}`);
+
 });
 
-//server is listening at http://127.0.0.1:8000
-server.listen(8000);
-console.log('Node.js web server at port 8000 is running.');
-
+//server is listening at http://127.0.0.1:5000
+server.listen(5000);
+console.log('Server is listening at http://127.0.0.1:5000');
+console.log( `Endpoints:`);
+console.log(`127.0.0.1:5000/method:GET`);
+console.log(`127.0.0.1:5000/images method:GET, POST, DELETE`);
